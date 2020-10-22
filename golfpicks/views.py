25831bb88a9a9147a9419458401ced343148dbfd
Diarmuid O'Neill <diarmuid.oneill@gmail.com>
2020-10-22 16:07:42 +0100

    update to new tourny

--- golfpicks/views.py
+++ golfpicks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from golfpicks.models import Pick
+from django.http import JsonResponse
 
 import requests
 import json
@@ -8,9 +9,14 @@ import json
 from golfpicks.models import Golfer, Pick, Punter, Event
 
 def get_score(pos):
+        if pos is None:
+            return 0
+        else:
+            print("Not none")
         return int(pos) if pos[0] != 'T' else int(pos[1:])
 
 def get_standing(pick, scores):
+    """Returnn a string comprised of golfers and their scores and the total score for a punter's picks"""
     standing = {'punter': pick.punter}
 
     golfers = [pick.picks.values()[0]['name'], pick.picks.values()[1]['name'], pick.picks.values()[2]['name']]
@@ -42,15 +48,13 @@ def index(request):
     
     # Available books (status = 'a')
 #    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    r =requests.get('https://www.golfchannel.com/api/v2/events/19163/leaderboard')
+    r =requests.get('https://www.golfchannel.com/api/v2/events/19164/leaderboard')
 
-    picks = Pick.objects.filter(event__external_id__exact=19163)
+    picks = Pick.objects.filter(event__external_id__exact=19164)
 
     my_json = json.loads(r.text)
 
     scores = dict([(n['firstName'] + ' ' + n['lastName'], get_score(n['position'])) for n in my_json['result']['golfers'] ])
-
-  #  standings = [dict((standing) for standing in picks]
 
     standings = sorted([get_standing(pick, scores) for pick in picks], key = lambda standing: standing['score'])
     
@@ -62,5 +66,11 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+def test(request):
+    picks = Pick.objects.all().values()  # or simply .values() to get all fields
+    pick_list = list(picks)  # important: convert the QuerySet to a list object
+    return JsonResponse(pick_list, safe=False)
+
 
 
