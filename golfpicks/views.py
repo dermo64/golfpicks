78dea72e28d195a58dd003724ca5f7ecb6300a46
Diarmuid O'Neill <diarmuid.oneill@gmail.com>
2020-11-11 15:40:02 +0000

    Masters!

--- golfpicks/views.py
+++ golfpicks/views.py
@@ -50,7 +50,7 @@ def index(request):
 #    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     r =requests.get('https://www.golfchannel.com/api/v2/events/18491/leaderboard')
 
-    picks = Pick.objects.filter(event__external_id__exact=19164)
+    picks = Pick.objects.filter(event__external_id__exact=18491)
 
     my_json = json.loads(r.text)
 
